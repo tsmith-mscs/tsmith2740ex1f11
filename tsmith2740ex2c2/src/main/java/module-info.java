a2f966f369c126c48eb2b97b5a7def6e47b7c1a4
Timothy Smith <zp7019rc@go.minnstate.edu>
2023-03-02 17:55:36 -0600

    tsmith2740ex2b1

---
+++ tsmith2740ex2c2/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tsmith2740ex2c2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.tsmith2740ex2c2 to javafx.fxml;
+    exports com.example.tsmith2740ex2c2;
+}
